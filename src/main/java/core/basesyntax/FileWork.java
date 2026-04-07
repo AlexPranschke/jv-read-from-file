0ba2449c1227dd3d819ca19533a5d378931e4f31
AlexPranschke <alexpranschke@gmail.com>
2026-04-07 23:10:31 +0200

    first

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,46 @@
 package core.basesyntax;
 
+import java.io.*;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
         //write your code here
-        return null;
+        int count = 0;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] words = line.split("\\s+");
+                for (String word : words) {
+                    word = word.replaceAll("[^a-zA-Z]", "").toLowerCase();
+                    if (!word.isEmpty() && word.charAt(0) == 'w') {
+                        count++;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new String[0];
+        }
+        String [] result = new String[count];
+        int index = 0;
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] words = line.split("\\s+");
+                for (String word : words) {
+                    word = word.replaceAll("[^a-zA-Z]", "").toLowerCase();
+                    if (!word.isEmpty() && word.charAt(0) == 'w') {
+                        result[index++] = word;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new String[0];
+        }
+        Arrays.sort(result);
+        return result;
     }
 }
